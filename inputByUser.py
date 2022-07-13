de1113d70b35ee3a0b89c03636074d081d57cb45
aradsheybak <arad.sheybak@outlook.com>
2022-07-13 12:21:56 +0430

    get a vlue from user

---
+++ inputByUser.py
@@ -0,0 +1,22 @@
+# in this section we want to get value from user input
+# we can get integer and string and other type of variable from user
+
+
+# 1: get a string from user:
+
+name = input("what is your name?:")
+print("you're name is : " + name)
+##################################################################
+
+# 2: get integer value from user:
+
+age = int(input("how old are you? "))
+print("you have " + str(age) + " years old ")
+
+###################################################################
+
+# 3: get float value from user
+average = float(input("what is your school average? : "))
+print("your average in school is: " + str(average))
+
+###################################################################
