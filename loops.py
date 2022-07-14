bfcd04e5d37f404174c44bb5479e3036e4547e19
aradsheybak <arad.sheybak@outlook.com>
2022-07-14 14:50:15 +0430

    for loop

--- loops.py
+++ loops.py
@@ -1,10 +1,20 @@
 # loops -> block of code who execute until a condition is true
 
 #######################################################################
-# 1: while=
+# 1: while
 count = int(input("Enter a number:"))
 
 while count > 0:
     print(count)
     count -= 1
 ########################################################################
+
+# 2: for
+
+for index in range(10, 101):
+    print(index)
+
+list_names = ["Arad", "Ali", "Roham", "niloufar"]
+
+for i in range(len(list_names)):
+    print(list_names[i])
