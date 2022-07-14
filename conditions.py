360129f299278989c1312e4227840413b62da286
aradsheybak <arad.sheybak@outlook.com>
2022-07-14 14:02:09 +0430

    condition if - else  - elif added

---
+++ conditions.py
@@ -0,0 +1,14 @@
+# if - else, elif(else if)
+
+age = int(input("how old are you?: "))
+
+if age >= 30:
+    print("you can sign up to this app!")
+
+elif 18 <= age < 30:
+    print("you can sign up in app but with limited accessibility! ")
+else:
+    print("you can't signup to this app!")
+
+########################################################################
+
