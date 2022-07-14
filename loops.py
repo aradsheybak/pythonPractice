c18adb805f1b8a6948da4f4f7428a29dc36adf24
aradsheybak <arad.sheybak@outlook.com>
2022-07-14 15:05:30 +0430

    control loop -> break added

--- loops.py
+++ loops.py
@@ -2,29 +2,40 @@
 
 #######################################################################
 # 1: while
-count = int(input("Enter a number:"))
-
-while count > 0:
-    print(count)
-    count -= 1
+# count = int(input("Enter a number:"))
+#
+# while count > 0:
+#     print(count)
+#     count -= 1
 ########################################################################
 
 # 2: for
 
-for index in range(10, 101):
-    print(index)
-
-list_names = ["Arad", "Ali", "Roham", "niloufar"]
-
-for i in range(len(list_names)):
-    print(list_names[i])
+# for index in range(10, 101):
+#     print(index)
+#
+# list_names = ["Arad", "Ali", "Roham", "niloufar"]
+#
+# for i in range(len(list_names)):
+#     print(list_names[i])
 ########################################################################
 # 3: nested loops
-row = int(input("How many row:"))
-columns = int(input("How many columns:"))
-
-for i in range(row):
-    for j in range(columns):
-        print("*", end="")
-    print()
+# row = int(input("How many row:"))
+# columns = int(input("How many columns:"))
+#
+# for i in range(row):
+#     for j in range(columns):
+#         print("*", end="")
+#     print()
 #######################################################################
+
+# 4: control loops -> break , continue, pass
+
+#break:
+
+while True:
+    name = str(input("Enter your name:"))
+    if name != "":
+        break
+
+
