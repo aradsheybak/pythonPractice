e8977e436ff921e0084dd5072fe7003a4468c05d
aradsheybak <arad.sheybak@outlook.com>
2022-07-14 14:09:06 +0430

    while loop

---
+++ loops.py
@@ -0,0 +1,10 @@
+# loops -> block of code who execute until a condition is true
+
+#######################################################################
+# 1: while=
+count = int(input("Enter a number:"))
+
+while count > 0:
+    print(count)
+    count -= 1
+########################################################################
