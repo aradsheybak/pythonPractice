b4f4f853aa6092359be713e7d330801e311b8be8
aradsheybak <arad.sheybak@outlook.com>
2022-07-16 11:49:32 +0430

    2D list completed

---
+++ 2DLists.py
@@ -0,0 +1,18 @@
+# 2D Lists -> a list in another list
+
+breakfast = ["egg", "cheese", "jar", "oatMeal"]
+lunch = ["rice", "meet", "burger", "spaghetti"]
+dinner = ["pizza", "tuna", "kebab"]
+
+restaurant_menu = [breakfast, lunch, dinner]
+
+print(restaurant_menu)
+##################################################################################
+# getting just 1 item
+
+print(restaurant_menu[0])
+##################################################################################
+
+#get 1 item from 1 list
+
+print(restaurant_menu[1][2])
