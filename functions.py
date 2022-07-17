71f11d9ff4531b76ecd3726e6838ead71eea5085
aradsheybak <arad.sheybak@outlook.com>
2022-07-17 23:39:55 +0430

    function completed

---
+++ functions.py
@@ -0,0 +1,15 @@
+# function = a block of code which is executed only when it is called
+
+def hello():
+    print("Hello!!!")
+
+
+hello()
+
+
+def sayHello(name, age):
+    print("Hello " + name)
+    print("You are " + str(age) + " years old")
+
+
+sayHello("Arad", 27)
