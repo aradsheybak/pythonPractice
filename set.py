d5f45d96283fe5ef7cce830032c2fa8777ae7a7d
aradsheybak <arad.sheybak@outlook.com>
2022-07-17 22:57:49 +0430

    set added

---
+++ set.py
@@ -0,0 +1,16 @@
+# set = collection which is unordered, unindexed, no duplicate value
+# set is faster mor than a list if you want to find a vlue is exist or not
+
+tools = {"hummer", "wrench", "pin","Benz"}
+cars = {"Benz", "BMW", "TOYOTA"}
+
+tools.add("meter")
+# tools.remove("hummer")
+# tools.clear()
+#tools.update(cars)
+#newSet = tools.union(cars)
+
+#print(tools.difference(cars))
+#print(tools.intersection(cars))
+#for x in newSet:
+# print(x)
